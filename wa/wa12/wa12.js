e826321cf5b2e95e9df54c920d870ae721b2bf21
reilley-locke <reilley.locke@colorado.edu>
2023-11-16 23:27:54 -0700

    wa12 submission

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,33 +1,91 @@
 // const newBtn = document.querySelector('#js-new-quote')   // Both work
 //const newQuote = document.getElementById('js-new-quote')
-const endPoint = 'https://x-colors.yurace.pro/api/random';
+
+const colorEndPoint = 'https://x-colors.yurace.pro/api/random';
 
 // Using separate function
-const newQuote = document.getElementById('js-new-quote').addEventListener('click', getQuote);
-async function getQuote() {     // async runs in parallel to other parts of the JS
+let newColor = document.getElementById('single-color').addEventListener('click', getColor);
+let newPalette = document.getElementById('palette').addEventListener('click', getPalette);
+let colorBoxes = document.querySelectorAll('.colorBox');
+let colorBoxesNames = document.querySelectorAll('.colorBoxName');
+
+function stopAnimation() {
+    let animationStop = document.getElementById('animation');
+    if (animationStop.checked == true)
+    {
+        document.querySelector('body').style.animation = 'none';
+    }
+    else
+    {
+        document.querySelector('body').style.animation = 'colorRotate 10s linear 0s infinite';
+    }
+}
+
+async function getColor() {     // async runs in parallel to other parts of the JS
+    document.querySelector('.paletteBox').style.visibility = "hidden";
+    document.querySelector('.paletteBoxNames').style.visibility = "hidden";
+    document.querySelector('.singleColorBox').style.visibility = "visible";
     console.log("Button works :)");
-    document.getElementById('js-answer-text').style.visibility = "hidden";
     try {   // Code we want to try to run
-        const response = await fetch(endPoint);
+
+        const response = await fetch(colorEndPoint);
         if (!response.ok) {     // If not status 200 (so 404, 303, etc.)
             throw Error(response.statusText);   //Catch bad response and throw an error
         }
         const json = await response.json();     // If response is good, then get the json data
-        console.log(json['hex']);  // Looks for question keyword; start with just json to see what it'll return
-        displayQuote(json['hex']);
+        
+        //console.log(json['hex']);  // Looks for question keyword; start with just json to see what it'll return
+        displayColor(json['hex']);
+        console.log(json);
     }
     catch(err) {    // Code to run if something goes run
         console.log(err);
         alert('Failed to fetch new quote');
     }
 };
-
-
-function displayQuote(hex) {
-    const questionTxt = document.querySelector('#js-quote-text');
-    questionTxt.textContent = hex;
-    questionTxt.style.color = hex;
-
+async function getPalette() {
+    document.querySelector('.paletteBox').style.visibility = "visible";
+    document.querySelector('.paletteBoxNames').style.visibility = "visible";
+    document.querySelector('.singleColorBox').style.visibility = "hidden";
+    console.log("Button works :)");
+    try {   // Code we want to try to run
+        const paletteEndPoint = 'https://corsproxy.io/?' + encodeURIComponent('https://www.colourlovers.com/api/palettes/random/new?format=json');
+        const response = await fetch(paletteEndPoint);
+        if (!response.ok) {     // If not status 200 (so 404, 303, etc.)
+            throw Error(response.statusText);   //Catch bad response and throw an error
+        }
+        const json = await response.json();     // If response is good, then get the json data
+        
+        //console.log(json['hex']);  // Looks for question keyword; start with just json to see what it'll return
+        for (let i = 0; i < json[0]['colors'].length; i++)
+        {
+            let color = '#' + json[0]['colors'][i];
+            console.log(color);
+            displayPalette(color, i);
+            //colorBoxes[i].style.color = '#' + json[0]['colors'][i];
+        }
+        //console.log(document.querySelector('.colorBox'));
+        //console.log(document.querySelectorAll('.colorBox')[0]);
+        
+        console.log(json);
+    }
+    catch(err) {    // Code to run if something goes run
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
 }
 
-getQuote();     // Runs the function once when the page loads
+// Single color function
+function displayColor(hex) {
+    const colorTxt = document.querySelector('#colorHex');
+    const colorSwatch = document.querySelector('#oneColorBox');
+    colorTxt.textContent = hex;
+    colorTxt.style.color = hex;
+    colorSwatch.style.backgroundColor = hex;
+}
+function displayPalette(color, i) {
+    colorBoxes[i].style.backgroundColor = color;
+    colorBoxesNames[i].textContent = color;
+}
+getColor();     // Runs the function once when the page loads
+//getPalette();

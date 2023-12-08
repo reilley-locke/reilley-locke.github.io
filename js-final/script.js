16ce5267c7b92f0a33e119b879b4d7493b792621
reilley-locke <reilley.locke@colorado.edu>
2023-12-08 16:36:38 -0700

    js final wip3

--- js-final/script.js
+++ js-final/script.js
@@ -1,7 +1,21 @@
+// Sliders and their # values
+// #1
 let slider1 = document.getElementById("bar1");
 let value1 = document.getElementById("num1");
+// #2
 let slider2 = document.getElementById("bar2");
 let value2 = document.getElementById("num2");
+
+let sliders = document.querySelectorAll(".slider");
+console.log(sliders);
+for (var i = 0; i < sliders.length; i++) {
+    let currentSlider = sliders[i];
+    console.log(currentSlider);
+}
+
+let rangeFill = document.querySelector('.rangefill');
+
+// Randomizer variables
 let randomizerNum = 0;
 let randomizerSign = 0;
 
@@ -13,17 +27,22 @@ slider1.oninput = function () {
     randomizerSign = Math.round(Math.random() * (2 - 1) + 1);
     console.log(randomizerSign);
     value1.innerHTML = this.value;
-    if (randomizerSign == 1) {
+    rangeFill.style.width = slider1.value + "%";
+
+    if (randomizerSign == 1) { // If the randomizer reads 1, set the randomizer negative
         randomizerNum = -(Math.round(Math.random() * (3 - 1) + 1));
-        console.log(randomizerNum);
     }
-    else {
+    else { // Else, make the randomizer positive
         randomizerNum = Math.round(Math.random() * (3 - 1) + 1);
     }
-    if (slider2.value + randomizerNum <= 9 || slider2.value + randomizerNum >= 0) {
 
+    console.log(randomizerNum + slider2.value);
+
+    // Check for if slider value is valid
+    // I love parseInt()!11!!!!1!
+    if (parseInt(slider2.value) + parseInt(randomizerNum) <= 9 && parseInt(slider2.value) + parseInt(randomizerNum) >= 0) {
         value2.innerHTML = parseInt(slider2.value) + parseInt(randomizerNum);
-        slider2.value = slider2.value + randomizerNum;
+        slider2.value = parseInt(slider2.value) + parseInt(randomizerNum);
     }
 
 }
